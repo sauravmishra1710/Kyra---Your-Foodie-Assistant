cf7153ea8e42f47cf08d8a945a63d5798cb16c2d
Saurav Mishra <saurav.mishra@live.com>
2021-11-11 18:45:06 +0530

    configuration params and chatbot endpoint

---
+++ config_params.py
@@ -0,0 +1,20 @@
+valid_cuisines = ['Chinese', 'Mexican', 'Italian', 'American', 'North Indian', 'South Indian']
+
+valid_budgets = ['Lesser than 300', '300 to 700', 'More than 700']
+
+user_key = {"user_key":"5aa9702bb8a9bbd3b5c5119bf71c0d05"}
+
+email,password = 'foodiebot669@gmail.com','foodie@123'
+
+email_template='''
+Hey Foodie,
+
+Here are the top rated restaurants in %s,
+
+%s
+
+Please do not reply on this email as it will go unanswered.
+
+Regards,
+Foodie Team
+'''
